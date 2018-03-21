9e57a300e2d0debd502a6812e7b5bf48f03140f9
Magaivaxe <mpg.salgad@gmail.com>
2018-03-21 18:42:10 -0400

    Menu Ok. Working on events

--- src/window/CaWindow.java
+++ src/window/CaWindow.java
@@ -5,13 +5,17 @@
  */
 package window;
 //Libraries
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 /**
  * Class to create the application window 
  * @author sire_marcos
  */
-public class CaWindow extends JFrame
+public class CaWindow extends JFrame implements ActionListener
 {
     //Fields
     private int X = 650;
@@ -43,12 +47,26 @@ public class CaWindow extends JFrame
         menuBar.add(mFile);menuBar.add(mEdit);
         menuBar.add(mTools);menuBar.add(mHelp);
         //MenuItems and add to menus
-        mFileNew = new JMenuItem("Nouveau"); mFile.add(mFileNew);
-        mFileOpen = new JMenuItem("Ouvrir"); mFile.add(mFileOpen);
-        mFileSave = new JMenuItem("Enregistrer"); mFile.add(mFileSave);
-        mFileSaveAs = new JMenuItem("Enregistrer sous"); mFile.add(mFileSaveAs);
-        mFileQuit = new JMenuItem("Fermer"); mFile.add(mFileQuit);
+        mFileNew = new JMenuItem("Nouveau");
+        mFile.add(mFileNew);
+        mFileNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+                                                       InputEvent.META_MASK));
+        mFileOpen = new JMenuItem("Ouvrir"); 
+        mFile.add(mFileOpen);
+        mFileOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
+                                                        InputEvent.META_MASK));
+        mFileSave = new JMenuItem("Enregistrer");
+        mFile.add(mFileSave);
+        mFileSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+                                                        InputEvent.META_MASK));
+        mFileSaveAs = new JMenuItem("Enregistrer sous...");
+        mFile.add(mFileSaveAs);
+        mFile.addSeparator();
         
+        mFileQuit = new JMenuItem("Fermer");
+        mFile.add(mFileQuit);
+        mFileQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+                                                        InputEvent.META_MASK));
         mEditAdd = new JMenuItem("Ajouter"); mEdit.add(mEditAdd);
         mEditDelete = new JMenuItem("Supprimer"); mEdit.add(mEditDelete);
         mEditModify = new JMenuItem("Modifier"); mEdit.add(mEditModify);
@@ -61,6 +79,14 @@ public class CaWindow extends JFrame
         mToolsClassify.add(mTClasStudent);
         mTClasLastName = new JMenuItem("Nom de famille");
         mToolsClassify.add(mTClasLastName);
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev)
+    {
+        
     }
 }
 
