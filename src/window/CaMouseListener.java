1b3d480c7337f6fd0f2c8fdb067f93fc51f8905b
Magaivaxe <mpg.salgad@gmail.com>
2018-03-21 17:37:44 -0400

    Menu Ok. Working on events

---
+++ src/window/CaMouseListener.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package window;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author sire_marcos
+ */
+public class CaMouseListener extends CaWindow implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent ev)
+    {
+        
+    }
+    
+}
---
+++ src/window/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package window;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sire_marcos
+ */
+public class Main
+{
+    //Instanciations
+    static JFrame cawindow = new CaWindow();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        //Visibility
+        cawindow.setVisible(true);
+    }
+    
+}
